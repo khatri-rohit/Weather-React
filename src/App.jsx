e8b2d7304228e703dc9f7b1c42af41946c353ebc
Rohit <rohitkhatri111112@gmail.com>
2024-06-20 18:24:02 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Error from './components/Error'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import { ThemeProvider } from './components/Theme-context'
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
