944f4ae265577d496e03d90764fe6aa021664306
Rohit <rohitkhatri111112@gmail.com>
2024-06-20 18:14:10 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Error from './components/Error'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import { ThemeProvider } from './components/Theme-context'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
